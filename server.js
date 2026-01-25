c2e50cfcc4d0c4b4584c611726d93a353437fec0
Mahmoud-Aldeeb <145771735+Mahmoud-Aldeeb@users.noreply.github.com>
2026-01-25 14:15:28 +0200

    Initial commit

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ const allowedOrigins = [
   "https://prescription-admin.vercel.app",
   "https://prescription-doctor.vercel.app",
   "http://localhost:3000",
+  "http://localhost:5173",
+  "http://localhost:5174",
   "http://localhost:3001",
   "http://localhost:3002",
 ];
