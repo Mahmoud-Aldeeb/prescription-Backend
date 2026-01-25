b7fb9b1506991091d76cce7ffbfb582dbf264ed1
Mahmoud-Aldeeb <145771735+Mahmoud-Aldeeb@users.noreply.github.com>
2026-01-25 08:03:57 +0200

    Initial commit

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,37 +1,42 @@
 import multer from "multer";
-import path from "path";
-import fs from "fs";
 const uploadsDir = "uploads";
-if (!fs.existsSync(uploadsDir)) {
-  fs.mkdirSync(uploadsDir, { recursive: true });
-}
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadsDir);
-  },
-  filename: (req, file, cb) => {
-    if (file) {
-      cb(null, new Date().toISOString().replace(/:/g, "-") + file.originalname);
-    } else {
-      cb(null, false);
-    }
-  },
-});
+// if (!fs.existsSync(uploadsDir)) {
+//   fs.mkdirSync(uploadsDir, { recursive: true });
+// }
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, uploadsDir);
+//   },
+//   filename: (req, file, cb) => {
+//     if (file) {
+//       cb(null, new Date().toISOString().replace(/:/g, "-") + file.originalname);
+//     } else {
+//       cb(null, false);
+//     }
+//   },
+// });
+// const storage = multer.memoryStorage();
 
-// const upload = multer({ storage });
+// const upload = multer({
+//   storage: storage,
+//   limits: { fileSize: 5 * 1024 * 1024 }, // 5MB max
+//   fileFilter: (req, file, cb) => {
+//     const allowedTypes = /jpeg|jpg|png|gif/;
+//     const mimetype = allowedTypes.test(file.mimetype);
+//     const extname = allowedTypes.test(
+//       file.originalname.toLowerCase().split(".").pop(),
+//     );
+
+//     if (mimetype && extname) {
+//       return cb(null, true);
+//     }
+//     cb(new Error("Only images are allowed"));
+//   },
+// });
+
 const upload = multer({
-  storage: storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB max
-  fileFilter: (req, file, cb) => {
-    const allowedTypes = /jpeg|jpg|png|gif/;
-    const mimetype = allowedTypes.test(file.mimetype);
-    const extname = allowedTypes.test(file.originalname.toLowerCase());
-
-    if (mimetype && extname) {
-      return cb(null, true);
-    }
-    cb(new Error("Only images are allowed"));
-  },
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 5 * 1024 * 1024 },
 });
 
 export default upload;
